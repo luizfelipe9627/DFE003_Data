e9b5e0aaac49267fec9abbf2b7d3baed463515f9
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-10-10 22:47:09 -0300

    feat: criado instruções iniciais.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// 1. Importar o arquivo JSON, chamar do HTML o container e estruturar o forEach.
+
+// 2. Criar os elementos HTML através do DOM utilizando JavaScript.
+
+// 3. Colocar o texto e a img do JSON nos elementos criados.
+
+// 4. Colocar os elementos criados dentro dos seus devidos pais(divs).
